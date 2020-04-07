a48882e73d43526be5f20bad745f48f6618187a1
Jan Borgersen <jborgersen@gmail.com>
2020-04-07 05:58:40 +0000

    undo test

--- src/AboutJan.js
+++ src/AboutJan.js
@@ -23,7 +23,7 @@ class AboutJan extends Component
     {
         return (
             <div className="normal-dark-mono-text scrollable">
-                <b>About Jan V3</b>
+                <b>About Jan</b>
                 <p>Jan is an engineer at <a href="https://www.wowza.com" target="_blank" rel="noopener noreferrer" onClick={ this.openPage('https://www.wowza.com')}>Wowza</a> working on <a href="https://www.wowza.com/products/clearcaster" target="_blank" rel="noopener noreferrer" onClick={ this.openPage('https://www.wowza.com/products/clearcaster')}>ClearCaster</a> and <a href="https://www.wowza.com/products/player" target="_blank" rel="noopener noreferrer" onClick={ this.openPage('https://www.wowza.com/products/player')}>Player</a>.</p>
                 <p>Previously, he led the UI Engineering team at <a href="https://www.zazzle.com" rel="noopener noreferrer" target="_blank" onClick={ this.openPage('https://www.zazzle.com')}>Zazzle</a>.</p>
                 <p>A long time ago, during the height of dot-com, he was a consultant with <b>Razorfish</b>.</p>
